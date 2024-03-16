1bd93c03bb23271b7c427c6bfe68e0660c183ad4
Hariesh S R <harieshsr@gmail.com>
2024-03-16 12:15:50 +0530

    er

--- script.js
+++ script.js
@@ -16,8 +16,34 @@ function d(){
         current.innerHTML=(Number(atd_hours)/(Number(max_hours))*100).toFixed(2)+`%`
     let after=document.getElementById("after_value");
 after.innerHTML=(Number(atd_hours)/(Number(max_hours)+Number(abs_hours))*100).toFixed(2)+`%`
+        if (abs_hours==''){
+            abs_hours=0
+            max_hours=Number(max_hours)
+        }
+        else{
+            max_hours=Number(max_hours)
+            max_hours+=Number(abs_hours)
+        }
+        let temp4 = document.getElementById("hours_value")
+        
+        let v = (Number(atd_hours)/max_hours)*100
+        if (v<75){
+            let i=0
+            while(true){
+                i+=1
+                let j = ((Number(atd_hours)+i)/(max_hours+i))*100
+                if (j>=75){
+                    break;
+                }
+            }
+            temp4.innerHTML= String(i)   
+        }
+        else{
+            temp4.innerHTML = "N/A"
+        }
+        
     }
-    
+
    
 
 }
